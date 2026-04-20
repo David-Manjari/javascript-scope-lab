146d674cd6f048900be6e4a772dabfb63482c922
Manjari <manjari@Manjari.localdomain>
2026-04-20 11:17:27 +0300

    Tried resolving errors coming from regex

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 // Write your solution in this file!
 
 const burgers = ['Hamburger', 'Cheeseburger']
-let featuredDrink = 'Strawberry Milkshake'
+let featuredDrink = "Strawberry Milkshake";
 
 function addBurger() {
     const newBurger = 'Flatburger';
     burgers.push(newBurger);
-   
+ if(true) { 
+    const anotherNewBurger = 'Maple Bacon Burger'; 
+    burgers.push(anotherNewBurger); 
+ } 
+  
 }
-addBurger();
- if(true){
-       const anotherNewBurger = 'Maple Bacon Burger';
-        burgers.push(anotherNewBurger);
-    }
-    
-function changeFeaturedDrink(){
-   featuredDrink = 'The java Shake';
-    
-} 
 
-changeFeaturedDrink();
-console.log (burgers);
-console.log(featuredDrink);
+
+
+function changeFeaturedDrink() {
+
+    featuredDrink = 'The JavaShake';
+
+}
+
+
+
+
